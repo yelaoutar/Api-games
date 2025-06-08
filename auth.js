41c66dbdedc06d924ccdf28207f53edf68212a7d
youssef <youssefotar@example.com>
2025-06-08 18:22:39 +0100

    fixed new feature

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 export default function authenticate(req,res,next){
     const authheader=req.headers["authorization"];
     const token=authheader&&authheader.split(" ")[1];
-    if(token==null)return res.send(401).json({access:"Unauthorized"})
+    if(token==null)return res.status(401).json({access:"Unauthorized"})
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
       if(err)return res.sendStatus(403)
       req.user=user
